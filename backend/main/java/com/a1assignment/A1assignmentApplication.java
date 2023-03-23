3aa71240df70031fc7637d4c82548ec875545678
vans <spentmny@nate.com>
2023-03-24 00:16:55 +0900

    project init

---
+++ backend/main/java/com/a1assignment/A1assignmentApplication.java
@@ -0,0 +1,13 @@
+package com.a1assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A1assignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A1assignmentApplication.class, args);
+	}
+
+}
---
+++ backend/test/java/com/a1assignment/A1assignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a1assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A1assignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
